c3beb507134b9813c6d8ccc4707a53bb443fe7a3
ZoOL <zhooul@gmail.com>
2023-08-02 14:53:09 +0800

    add some doc

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,19 @@ use chrono::DateTime;
 use cron::Schedule;
 
 /// Creates a closure that checks if the cron expression has passed
+/// # expression format:
+/// | sec  | min  | hour | day of month | month | day of week | year      |
+/// |------|------|------|--------------|-------|-------------|-----------|
+/// | *    | *    | *    | *            | *     | *           | *         |
+/// | 0-59 | 0-59 | 0-23 | 1-23         | 1-12  | 1-7         | 1970-2100 |
+///
+/// # example:
+/// | expression | description|
+/// |------|------|
+/// |0/5 * * * * * * | every 5 sec|
+/// |0 * * * * * * | every minute |
+/// |0 5,10 * * * * * | every hour on 5 and 10 min|
+/// |0 0 1 * * * * | every day on 1:00:00|
 pub fn schedule_passed(expression: &str) -> impl FnMut(Local<Option<DateTime<chrono::Utc>>>) -> bool {
     let schedule = Schedule::from_str(expression).expect("Failed to parse cron expression");
     move |mut local_schedule: Local<Option<DateTime<chrono::Utc>>>| {
